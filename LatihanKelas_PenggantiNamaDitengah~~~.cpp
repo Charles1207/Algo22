1e02d88d49fa76687962de2ecfbb56d1e162d5c4
Charles1207 <114063953+Charles1207@users.noreply.github.com>
2022-09-27 19:11:06 +0700

    algo2022

---
+++ LatihanKelas_PenggantiNamaDitengah~~~.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+	string nama;
+	
+	cout<<"masukkan nama anda : ";
+	getline(cin,nama);
+	cout<<"--------------------------------"<<endl;
+	cout<<"Selamat "<<nama<< " anda keren";
+	
+
+	return 0;
+}
+
